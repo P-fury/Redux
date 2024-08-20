4ee8fae84827cf765cb60f16b2d0a59c9541724a
Piotr Plucinski <plutocamera@gmail.com>
2024-08-20 12:42:59 +0200

    3.1 EX: 1 v.2 DONE

--- 03_Dzien_4/01_Komponenty/01_Zadanie_1/js/hoc/withTimer.js
+++ 03_Dzien_4/01_Komponenty/01_Zadanie_1/js/hoc/withTimer.js
@@ -5,30 +5,63 @@
  * export default withTimer;
  */
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 
+
+// export default function withTimer(Component) {
+//     return class extends React.Component {
+//         state = {
+//             counter: 0
+//         }
+//
+//
+//         startTimer = () => {
+//             this.interval = setInterval(() => {
+//                 this.setState(prevState => ({
+//                     counter: prevState.counter + 1
+//                 }));
+//             }, 1000);
+//         }
+//         stopTimer = () => {
+//             clearInterval(this.interval)
+//         }
+//
+//         render() {
+//             return <Component {...this.props} {...this.state} startTimer={this.startTimer} stopTimer={this.stopTimer} />
+//         }
+//     }
+//
+// }
 
 export default function withTimer(Component) {
-    return class extends React.Component {
-        state = {
-            counter: 0
-        }
+    return function (props) {
+        const [counter, setCounter] = useState(0)
+        const [isCounting, setIsCounting] = useState(false)
 
+        const startTimer = () => {
+            setIsCounting(true)
+        };
 
-        startTimer = () => {
-            this.interval = setInterval(() => {
-                this.setState(prevState => ({
-                    counter: prevState.counter + 1
-                }));
-            }, 1000);
-        }
-        stopTimer = () => {
-            clearInterval(this.interval)
-        }
+        const stopTimer = () => {
+            // clearInterval(interval.current);
+            setIsCounting(false)
+        };
 
-        render() {
-            return <Component {...this.props} {...this.state} startTimer={this.startTimer} stopTimer={this.stopTimer} />
-        }
+        useEffect(() => {
+            let interval;
+            if (isCounting) {
+                interval = setInterval(() => {
+                    setCounter(prevCounter => prevCounter + 1);
+                }, 1000)
+            }
+            return () => {
+                clearInterval(interval);
+            }
+
+        }, [isCounting]);
+
+    return(
+        <Component {...props} counter={counter} startTimer={startTimer} stopTimer={stopTimer} />
+    )
     }
-
 }
